e4075abd3b13894ef72da46faf5ea6ca7b7a469e
Maxim Novak <118775465+novamax24@users.noreply.github.com>
2023-01-09 13:17:14 -0800

    Create Game.java
    .

---
+++ Game.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Game implements Runnable {
+    public void run() {
+        SwingUtilities.invokeLater(new StartMenu());
+    }
+    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Game());
+    }
+}
+//TODO: Comment / revariable / reformat
+//TODO: Fix checkmate
+//TODO: piece sticking to mouse when drag
+//
